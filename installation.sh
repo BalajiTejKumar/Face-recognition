32f8aa477995049063d3b7e181ccaf357f500b2b
BalajiTejKumar <balaji.643@gmail.com>
2015-08-21 20:33:01 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-pip
+sudo pip install matplotlib
+sudo apt-get install build-essential python-dev python-setuptools \
+                     python-numpy python-scipy \
+                     libatlas-dev libatlas3gf-base
+
+
+sudo pip install scikit-learn
